388968de50bebd603f70072326a13a06dc10583d
kittipong582 <taopung_tao@hotmail.com>
2025-02-04 14:44:25 +0700

    commit index.php in main branch

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
       <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
       <small id="helpId" class="text-muted">Help text</small>
     </div>
+    <div class="form-group">
+      <label for=""></label>
+      <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+      <small id="helpId" class="text-muted">Help text</small>
+    </div>
 </body>
 </html>
 <script>
