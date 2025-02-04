36b859709061f14346cee550dcd410e22d4595bb
kittipong582 <taopung_tao@hotmail.com>
2025-02-04 14:51:20 +0700

    update function gettable

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
+<div class="ibox">
+    <div class="ibox-title">
+        <div class="row">
+            <div class="col-lg-3 mb-2">
+                <h3>TITLE</h3>
+            </div>
+        </div>
+        <div class="ibox-tools">
+            <button class="btn btn-md btn-primary" onclick="form_add()"><i class="fa fa-plus"></i>&nbsp;เพิ่มบทความ</button>
+        </div>
+    </div>
+    <div class="ibox-content">
+        <!----- ตาราง ---->
+        <div id="Loading">
+            <div class="spiner-example">
+                <div class="sk-spinner sk-spinner-wave">
+                    <div class="sk-rect1"></div>
+                    <div class="sk-rect2"></div>
+                    <div class="sk-rect3"></div>
+                    <div class="sk-rect4"></div>
+                    <div class="sk-rect5"></div>
+                </div>
+            </div>
+        </div>
+        <div id="show_table"></div>
+        <!----- ตาราง ---->
+    </div>
+</div>
 <script>
     $(document).ready(function () {
-        
+        GetTable()
     });
 
     function GetTable(){
@@ -10,7 +38,7 @@
             data: "data",
             dataType: "dataType",
             success: function (response) {
-                
+                $("#show_table").html(response);
             }
         });
     }
